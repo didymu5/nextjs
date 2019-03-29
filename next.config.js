393a378ce4a29a4f9e14b2bb33f5e7cd8cc361dc
Thomas X. Wu <thomas.wu@kp.org>
2019-03-29 13:26:32 -0700

    added semantic and css support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS({
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          publicPath: "/_next/static/",
+          outputPath: "static/",
+          name: "[name].[ext]"
+        }
+      }
+    });
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty"
+    };
+    return config;
+  },
   target: 'serverless'
-}
+});
